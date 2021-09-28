8161df5623809ba1acf472c5474cabb8fca8efec
Dương Quốc Vương <70870745+DuongVuongUTE@users.noreply.github.com>
2021-09-28 15:11:43 +0700

    update

--- src/redux/sagas/apiUrl.js
+++ src/redux/sagas/apiUrl.js
@@ -1,2 +1,2 @@
-export const SERVER_API_URL =
-  "http://localhost:3001" || "https://api-vuong-demo.herokuapp.com";
+export const SERVER_API_URL ="https://api-vuong-demo.herokuapp.com" ||
+  "http://localhost:3001" ;
